d6381556dda617cc05767a49569db953db52c9eb
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-02-14 11:24:42 -0300

    Create WeirdAlgorithm.cpp

---
+++ CSES/WeirdAlgorithm.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long N; cin >> N;
+    printf("\n%lld ", N);
+    while(N != 1){
+        if(N == 0) break;
+        if(N % 2 == 1){
+            N = N * 3 + 1;
+            printf("%lld ", N);
+        }
+        else{
+            N /= 2;
+            printf("%lld ", N);
+        }
+    }
+    return 0;
