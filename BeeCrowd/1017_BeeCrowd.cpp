84117b777aeda3a8c3d48b4a8e5de2051f6df4b7
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-02-24 20:05:03 -0300

    Delete BeeCrowd/1017_BeeCrowd.cpp

--- BeeCrowd/1017_BeeCrowd.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
-int main() {
-    int SpentTime, AvgSpeed;
-    scanf("%d", &SpentTime);
-    scanf("%d", &AvgSpeed);
-    printf("%.3f\n", (SpentTime/12.0)*AvgSpeed);
-    return 0;
-}
