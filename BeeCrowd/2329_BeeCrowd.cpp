c0ead21b750a903adebd0faf835d6007387b496e
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-02-26 15:08:04 -0300

    Update 2329_BeeCrowd.cpp

--- BeeCrowd/2329_BeeCrowd.cpp
+++ BeeCrowd/2329_BeeCrowd.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
+#define pb push_back
+typedef long long ll;
 
-int Contador_Fatias(int N, int Vet[]){
-    int aux = 0;
+int N, K, m;
+ll T[10000] = {0};
+
+int Cut_Bread(int m){
+    int S = 0;
     for (int i = 0; i < K; i++)
-        aux += Vet[i] / N;
-    return aux;
+        S += T[i] / m;
+    return S;
 }
 
-int Busca_Valor(int Vet[]){
-    int esq = 0, dir = 10000, meio;
-    while (esq < dir - 1)
+int Binary_Search(){
+    int l = 0, r = 10000;
+    while(l < r - 1)
     {
-        meio = (esq+dir)/2;
-        if(Contador_Fatias(meio, Vet) < N)
-            dir = meio;
-        if(Contador_Fatias(meio, Vet) >= N)
-            esq = meio;
+        m = (l+r)/2;
+        if(Cut_Bread(m) < N)
+            r = m;
+        else
+            l = m;
     }
-    return esq;
+    return l;
 }
 
 int main(){
     scanf("%d", &N);
     scanf("%d", &K);
-    int Vet[K];
     for (int i = 0; i < K; i++)
-        scanf("%d", &Vet[i]);
-    printf("%d\n", Busca_Valor(Vet));
+        scanf("%lld", &T[i]);
+    printf("%d\n", Binary_Search());
     return 0;
 }
