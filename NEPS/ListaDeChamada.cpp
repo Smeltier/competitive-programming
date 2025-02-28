91287f813694fe3a2d799dd3164af9e2bcd74949
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-02-28 12:53:58 -0300

    Create ListaDeChamada.cpp

---
+++ NEPS/ListaDeChamada.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, K;
+    set<string> nome;
+    string aux;
+
+    scanf("%d %d", &N, &K);
+    cin.ignore(); 
+
+    for (int i = 0; i < N; i++) {
+        getline(cin, aux); 
+        nome.insert(aux);  
+    }
+    
+    auto it = nome.begin();
+    advance(it, K - 1);
+
+    cout << *it << endl;
+
+    return 0;
+}
