61c9ba43da66be760e3f5f0cf88a5327f79ed412
Gabriel Gonçalves <140024682+Smeltier@users.noreply.github.com>
2025-03-15 14:48:22 -0300

    Create 1000_BeeCrowd.cpp

---
+++ BeeCrowd/1000_BeeCrowd.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define endl "\n"
+
+typedef long long ll;
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    cout << "Hello World!" << endl;
+
+}
