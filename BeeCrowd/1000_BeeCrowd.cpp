b045c5030c28f8393bf3cb0c99aad35c6585c1b1
Smeltier <connect.goncalves@gmail.com>
2025-05-21 13:26:08 -0300

    Codigo CodeForces

--- BeeCrowd/1000_BeeCrowd.cpp
+++ BeeCrowd/1000_BeeCrowd.cpp
@@ -16,10 +16,12 @@ int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("/input.txt", "r", stdin);
+    freopen("/output.txt", "w", stdout);
 #endif
 
-    cout << "Hello World!" << endl;
+    int x;
+    cin >> x;
+    cout << x << " " << "Hello World!" << endl;
 
 }
